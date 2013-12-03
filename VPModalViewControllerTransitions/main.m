926bfaa92ba9f70b391cdc8643c8c4245cf80e5e
Vidu Pirathaparajah <writetovidu@gmail.com>
2013-12-03 03:30:38 +0100

    Initial Commit

---
+++ VPModalViewControllerTransitions/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VPModalViewControllerTransitions
+//
+//  Created by Vidu Pirathaparajah on 03/12/13.
+//  Copyright (c) 2013 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VPAppDelegate class]));
+    }
+}
---
+++ VPModalViewControllerTransitionsTests/VPModalViewControllerTransitionsTests.m
@@ -0,0 +1,34 @@
+//
+//  VPModalViewControllerTransitionsTests.m
+//  VPModalViewControllerTransitionsTests
+//
+//  Created by Vidu Pirathaparajah on 03/12/13.
+//  Copyright (c) 2013 Vidu Pirathaparajah. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VPModalViewControllerTransitionsTests : XCTestCase
+
+@end
+
+@implementation VPModalViewControllerTransitionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
